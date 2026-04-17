0e1fec3caa73cd8034836bb73aacfce1381340b2
Lucas Gonçalves <glucassilva961@gmail.com>
2026-04-17 10:19:27 -0300

    remover gasto

--- Controle Flux/app.js
+++ Controle Flux/app.js
@@ -1,10 +1,32 @@
 let gastos = []; //chaves sem nada porque o usuario ainda ira adicionar
 let total = 0; //comeca em zero
 let graficoInstancia = null
+const dadosSalvos = localStorage.getItem("gastos") // salvando dados
+
+    //ira salvar os dados dos usuarios mesmo quando eles recarregarem a pagina
+    if (dadosSalvos) {
+    gastos = JSON.parse(dadosSalvos)
+
+    for (const gasto of gastos){
+        const listaGastosTotal = document.createElement("li")
+        listaGastosTotal.innerHTML = `${gasto.categoria} - ${gasto.descricao} = R$ ${gasto.valor.toFixed(2)}`
+        const btnRemover = document.createElement("button")
+        btnRemover.addEventListener("click", function(){
+        listaGastosTotal.remove()
+        gastos.splice(indice, 1)
+    atualizarGrafico()
+    })
+        btnRemover.textContent = "X"
+        listaGastosTotal.appendChild(btnRemover)
+        document.getElementById("listaGastos").appendChild(listaGastosTotal)
+    }}
+
 
 //ocorrer evento onde o botao funcionara quando for clicado
 const btn = document.getElementById("adicionarGasto")
 btn.addEventListener("click", function() {
+    
+    //verificando se o campo esta vazio, se estiver o site nao dara funcionamento
     if ((document.getElementById('descricao').value.trim() === "") ||(document.getElementById('valor').value.trim() === "")) {
         window.alert("O campo esta vazio!")
         return 0;
@@ -18,12 +40,15 @@ btn.addEventListener("click", function() {
     
     total = valor + total
 
-    document.getElementById('categoria').selectedIndex = 0
-
     const categoria = document.getElementById('categoria').value
+    
+    //categoria sempre ira começar com a primeira da lista
+    document.getElementById('categoria').selectedIndex = 0
 
     //array onde tudo do gasto sera o que foi adicinado do valor e descricao
     gastos.push({valor, descricao, categoria})
+    console.log("salvando...", gastos)
+    localStorage.setItem("gastos", JSON.stringify(gastos))
 
     const indice = gastos.length - 1
 
@@ -38,12 +63,14 @@ btn.addEventListener("click", function() {
     //pegando a variavel e juntando as variaveis do html na variavel do js
     listaGastosTotal.innerHTML = `${categoria} - ${descricao} = R$ ${valor.toFixed(2)}`
 
+    //botao X para remover o gasto no site
     listaGastosTotal.appendChild(btnRemover)
     
     btnRemover.addEventListener("click", function(){
         listaGastosTotal.remove()
         gastos.splice(indice, 1)
         
+    //atualiza o grafico sempre que for adicionar um gasto
         atualizarGrafico()
     })
     
@@ -59,6 +86,7 @@ function atualizarGrafico() {
     const labels = []
     const valores = []
 
+    //percorre todo array
     for (const gasto of gastos) {
         const posicao = labels.indexOf(gasto.categoria)
 
@@ -86,4 +114,5 @@ function atualizarGrafico() {
         }
     })
 }
+
 
